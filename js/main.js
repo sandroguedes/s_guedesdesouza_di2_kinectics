9e2b93d97ac76d945269a5afbab1bdbd70040428
sandroguedes <59712770+sandroguedes@users.noreply.github.com>
2020-08-01 23:19:21 -0400

    Update to Animation
    Completed the animation for the texts. Viewport still doesn't move.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 (() => {
+  console.log('you wake up and and go into a state of horror as you notice the horse head on your left hand side');
+
   let audio = document.querySelector("audio"),
       playButton = document.querySelector(".controls button");
 
   function playAudio() {
     document.querySelectorAll("path").forEach(path => path.classList.add("textanimation"))
+    document.querySelectorAll("rect").forEach(rect => rect.classList.add("textanimation"))
     document.querySelector("#svgcontainer").classList.add("containeranimation")
     audio.play()
   }
